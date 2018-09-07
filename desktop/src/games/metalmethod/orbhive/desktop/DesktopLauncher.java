09efec153bb5ce76b1ccf02e7612b4c33e383b65
Igor Busquets <igorbusquets@gmail.com>
2018-09-07 22:09:13 +0100

    Ädded core files

---
+++ desktop/src/games/metalmethod/orbhive/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package games.metalmethod.orbhive.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import games.metalmethod.orbhive.OrbHiveGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OrbHiveGame(), config);
+	}
+}
